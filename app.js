13ae5c12d9cd6f903356761390021d8451decf43
Elaine Ou <elaine.ou@gmail.com>
2016-01-31 18:33:51 -0800

    test again

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 var express = require('express');
 var path = require('path');
 var gpio = require("pi-gpio");
@@ -17,21 +16,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-// routes
-
-app.get('/', function(req, res) {
-  res.render('index');
+gpio.open(1, "output", function(err) {     // Open pin 16 for output
+    gpio.write(1, 1, function() {          // Set pin 16 high (1)
+        console.log("bitcoin received");
+        gpio.close(1);                     // Close pin 16
+    });
 });
 
-
-
-
-gpio.open(16, "output", function(err) {     // Open pin 16 for output
-    gpio.write(16, 1, function() {          // Set pin 16 high (1)
-        console.log("hello")
-        gpio.close(16);                     // Close pin 16
-    });
+// routes
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
 
