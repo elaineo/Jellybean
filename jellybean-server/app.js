18f3b470890c6ee767fe1e006a3b87ac40a0e569
Elaine Ou <elaine.ou@gmail.com>
2016-02-01 09:48:24 -0800

    serve a web page

--- jellybean-server/app.js
+++ jellybean-server/app.js
@@ -1,17 +1,30 @@
 /*
     Server to monitor network and call client when appropriate
 */
-"use strict";
-
-
 var WebSocketServer = require('ws').Server;
-var http = require('http');
 var url = require('url');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 
-var server = http.createServer(function(request, response) {
-    // process HTTP request. Since we're writing just WebSockets server
-    // we don't have to implement anything.
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// serve a page
+app.get('/', function(req, res){
+  res.render('index.html', { title: 'Watson\'s Book Shop' });
 });
+
+var server = require('http').createServer(app);
+
 server.listen(3000, function() { 
     console.log((new Date()) + " Server is listening on port 3000");
 });
