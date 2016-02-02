ae8a9ef8d4998ce3c9b2e6c44e1f53209eb86ba1
Elaine Ou <elaine.ou@gmail.com>
2016-02-02 01:06:28 -0800

    better keepalive

--- app.js
+++ app.js
@@ -20,46 +20,14 @@ else {
   var wsurl = 'ws://54.174.77.180';
 }
 
-var connection = new WebSocket(wsurl);
+var connection;
+openSocket(0);
 
 app.set('port', process.env.PORT || 4000);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-connection.on('open', function () {
-  connection.send('Hello from client');
-});
-
-connection.on('close', function () {
-  console.log('closed, reconnecting');
-  setTimeout (keepAlive(0), PING_TIME);
-});
-
-connection.on('ping', function () {
-  console.log('ping');
-});
-
-connection.on('error', function (error) {
-  console.log(error);
-  setTimeout (keepAlive(0), PING_TIME);
-});
-
-connection.on('message', function(data, flags) {
-  console.log(data);
-  
-  // not on the pi
-  if ('test' == app.get('env')) return;
-
-  gpio.open(16, "output", function(err) {     // Open pin 16 for output
-    gpio.write(16, 1, function() {          // Set pin 16 high (1)
-        console.log("received something");
-        setTimeout(stopMotor,5000);
-      });
-    });
-});
-
 
 // debug 
 function stopMotor () {
@@ -72,7 +40,7 @@ http.listen(app.get('port'), '0.0.0.0', function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
-function keepAlive(reconnectAttempts){
+function openSocket(reconnectAttempts){
   if (reconnectAttempts > MAX_RETRIES) {
     console.log('Max retries reached');
     return
@@ -84,17 +52,39 @@ function keepAlive(reconnectAttempts){
   var timeout = setTimeout(function() {
                   console.log('ReconnectingWebSocket timed out');
                   localConn.close();
-                  keepAlive(reconnectAttempts++);
+                  openSocket(reconnectAttempts++);
                 }, PING_TIME*10);
 
   connection.on('open', function() {
     clearTimeout(timeout);
-    console.log('Reconnected')
+    console.log('Connected');
+    connection.send('Hello from client');
   });
+
+  connection.on('message', function(data, flags) {
+    console.log(data);
+    
+    // not on the pi
+    if ('test' == app.get('env')) return;
+
+    gpio.open(16, "output", function(err) {     // Open pin 16 for output
+      gpio.write(16, 1, function() {          // Set pin 16 high (1)
+          console.log("received something");
+          setTimeout(stopMotor,5000);
+        });
+      });
+  });
+  connection.on('ping', function () {
+  });
+
   connection.on('error', function (error) {
     clearTimeout(timeout);
     var longTime = PING_TIME * Math.pow(DELAY_TIME, reconnectAttempts);
-    setTimeout (keepAlive(reconnectAttempts++), longTime);
+    setTimeout (openSocket(reconnectAttempts++), longTime);
   });
 
+  connection.on('close', function () {
+    console.log('closed, reconnecting');
+    setTimeout (openSocket(0), PING_TIME);
+  });
 }
