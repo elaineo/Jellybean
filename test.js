f118ae40b9f9a27aa26360552b32236240e8b27d
Jesus <elaine.ou@gmail.com>
2016-02-03 17:04:35 +0000

    test update

--- test.js
+++ test.js
@@ -1,15 +1,19 @@
 /*
     GPIO test */ var gpio = require("pi-gpio");
 
-var p = 18;
-gpio.close(p);
+var p = 18; gpio.close(p);
+
+function startUp() {
   gpio.open(p, "output", function(err) {
     gpio.write(p, 1, function() {
       console.log("writing to " + p);
       setTimeout(function() { cleanup(p) }, 10000);
     });
   });
-
+}
 function cleanup(q) {
   gpio.close(q);
 }
+
+startUp(16);;
+startUp(18);
