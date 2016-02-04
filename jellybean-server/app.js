b9e6f70492a13cfe1430a47bd32fcade933faaec
Elaine Ou <elaine.ou@gmail.com>
2016-02-03 18:24:58 -0800

    err logging for connection

--- jellybean-server/app.js
+++ jellybean-server/app.js
@@ -36,11 +36,12 @@ else {
 var server = http.createServer(app);
 
 var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+web3.setProvider(new web3.providers.HttpProvider('http://54.174.77.180:8545'));
 console.log(web3)
 //var coinbase = web3.eth.coinbase;
 //var balance = web3.eth.getBalance(coinbase);
-if(!web3.isConnected()) console.log("bad connection")
+if(!web3.isConnected()) console.log("bad connection");
+else console.log("successful connection");
 
 server.listen(app.get('port'), function() { 
     console.log((new Date()) + " Server is listening on port " + app.get('port'));
