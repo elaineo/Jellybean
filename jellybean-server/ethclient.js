fa5cdd934a0c707cdc8de8f6f8eb2b84605f410b
Elaine Ou <elaine.ou@gmail.com>
2016-02-07 16:36:25 -0800

    test client

--- jellybean-server/ethclient.js
+++ jellybean-server/ethclient.js
@@ -2,8 +2,85 @@ var Web3 = require('web3');
 
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://54.174.77.180:8545'));
-console.log(web3)
-//var coinbase = web3.eth.coinbase;
-//var balance = web3.eth.getBalance(coinbase);
+
+
 if(!web3.isConnected()) console.log("bad connection");
 else console.log("successful connection");
+
+var coinbase = web3.eth.coinbase;
+var balance = web3.eth.getBalance(coinbase);
+console.log(balance.toString(10));
+
+var abi = [{
+        constant: false,
+        inputs: [],
+        name: "consumeTurnMicroseconds",
+        outputs: [{
+            name: "d",
+            type: "uint256"
+        }],
+        type: "function"
+    }, {
+        constant: true,
+        inputs: [],
+        name: "lastTurnMicroseconds",
+        outputs: [{
+            name: "",
+            type: "uint256"
+        }],
+        type: "function"
+    }, {
+        constant: true,
+        inputs: [],
+        name: "mostRecentPaid",
+        outputs: [{
+            name: "",
+            type: "uint256"
+        }],
+        type: "function"
+    }, {
+        constant: false,
+        inputs: [],
+        name: "getTurnMicroseconds",
+        outputs: [{
+            name: "d",
+            type: "uint256"
+        }],
+        type: "function"
+    }, {
+        constant: false,
+        inputs: [],
+        name: "payForBeans",
+        outputs: [{
+            name: "d",
+            type: "uint256"
+        }],
+        type: "function"
+    }, {
+        constant: true,
+        inputs: [],
+        name: "turnMicroseconds",
+        outputs: [{
+            name: "",
+            type: "uint256"
+        }],
+        type: "function"
+    }, {
+        constant: false,
+        inputs: [],
+        name: "getMostRecentPaid",
+        outputs: [{
+            name: "d",
+            type: "uint256"
+        }],
+        type: "function"
+    }, {
+        inputs: [],
+        type: "constructor"
+    }];
+
+var beansAddress = "0x06fccdfc1b767b9ac654b366c4a915628dda98a5"
+var beans = web3.eth.contract(abi).at(beansAddress);
+
+var price = beansContract.mostRecentPaid();
+console.log(price)
